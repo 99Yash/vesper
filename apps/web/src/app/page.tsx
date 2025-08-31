5d8fbb07fc7276aab305ed82250cbc50907e6349
yash <yashgouravkar@gmail.com>
2025-08-31 12:27:38 +0530

    ss

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
 import { redirect } from "next/navigation";
 import { Button } from "~/components/ui/button";
-import { trpc } from "~/utils/trpc";
 import { authClient } from '../lib/auth-client';
 
 
@@ -12,9 +10,6 @@ export default function Home() {
 	if (!data)  {
 		redirect("/signin");
 	}
-	const healthCheck = useQuery(trpc.privateData.queryOptions(undefined,{
-		refetchOnWindowFocus: false,
-	}));
 
 	return (
 		<div className="container mx-auto max-w-3xl px-4 py-2">
@@ -23,14 +18,10 @@ export default function Home() {
 					<h2 className="mb-2 font-medium">API Status</h2>
 					<div className="flex items-center gap-2">
 						<div
-							className={`h-2 w-2 rounded-full ${healthCheck.isSuccess ? "bg-green-500" : "bg-red-500"}`}
+							className={`h-2 w-2 rounded-full ${data.user ? "bg-green-500" : "bg-red-500"}`}
 						/>
 						<span className="text-sm text-muted-foreground">
-							{healthCheck.isPending
-								? "Checking..."
-								: healthCheck.isSuccess
-									? "Connected"
-									: "Disconnected"}
+							{data.user.name}
 						</span>
 					</div>
 					<div className="flex items-center gap-2">
