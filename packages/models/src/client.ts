2ce7c1560f7032665e6f0c9ae8552f6c28a19a9a
yash <yashgouravkar@gmail.com>
2025-09-17 17:40:43 +0530

    imp

--- packages/models/src/client.ts
+++ packages/models/src/client.ts
@@ -3,7 +3,7 @@ export * from "replicache";
 export * from "./cvr";
 export * from "./err";
 export * from "./idb-key";
-export * from "./mutator";
+export * from "./mutators";
 export * from "./validators";
 
 // Re-export types only from db (no implementation that might include server deps)
